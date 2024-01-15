1e7ca9a466a93b2932c0903030eed26d99acbe40
sourceTree <sourcetree@mail.com>
2024-01-15 00:17:33 -0700

    change hbm2ddl.auto to update

--- java/org/koushik/hibernate/HibernateTest.java
+++ java/org/koushik/hibernate/HibernateTest.java
@@ -7,8 +7,8 @@ import org.javabrains.koushik.dto.UserDetails;
 public class HibernateTest {
     public static void main(String[] args) {
         UserDetails userDetails = new UserDetails();
-        userDetails.setUserId(2);
-        userDetails.setUserName("Second User");
+        userDetails.setUserId(3);
+        userDetails.setUserName("Third User");
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
