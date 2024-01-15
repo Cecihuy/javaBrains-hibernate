c28e1fc429163cf0934e16396a5257ec139db38f
sourceTree <sourcetree@mail.com>
2024-01-15 00:32:32 -0700

    change column name for database

--- java/org/javabrains/koushik/dto/UserDetails.java
+++ java/org/javabrains/koushik/dto/UserDetails.java
@@ -1,11 +1,14 @@
 package org.javabrains.koushik.dto;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity(name = "USER_DETAILS")
 public class UserDetails{
     @Id
+    @Column(name = "USER_ID")
     private int userId;
+    @Column(name = "USER_NAME")
     private String userName;
 
     public int getUserId() {
