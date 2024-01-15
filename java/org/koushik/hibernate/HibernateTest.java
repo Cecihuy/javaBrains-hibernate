da932958a40751dc271aa6b046fb33e28088e320
sourceTree <sourcetree@mail.com>
2024-01-15 00:07:29 -0700

    03 Part 3- Saving Objects using Hibernate APIs (reconstruct project folder)

---
+++ java/org/koushik/hibernate/HibernateTest.java
@@ -0,0 +1,19 @@
+package org.koushik.hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.javabrains.koushik.dto.UserDetails;
+
+public class HibernateTest {
+    public static void main(String[] args) {
+        UserDetails userDetails = new UserDetails();
+        userDetails.setUserId(1);
+        userDetails.setUserName("First User");
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.persist(userDetails);
+        session.getTransaction().commit();
+    }
+}
