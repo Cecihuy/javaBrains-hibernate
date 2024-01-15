627804c029d5726d586a485812ccdbe48bcf1d00
sourceTree <sourcetree@mail.com>
2024-01-15 00:37:42 -0700

    put annotation on top of getter for hibernate to pull values from getter

--- java/org/javabrains/koushik/dto/UserDetails.java
+++ java/org/javabrains/koushik/dto/UserDetails.java
@@ -4,21 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity(name = "USER_DETAILS")
-public class UserDetails{
-    @Id
-    @Column(name = "USER_ID")
-    private int userId;
-    @Column(name = "USER_NAME")
+public class UserDetails{    
+    private int userId;    
     private String userName;
 
+    @Id
+    @Column(name = "USER_ID")
     public int getUserId() {
         return userId;
     }
     public void setUserId(int userId) {
         this.userId = userId;
     }
+    @Column(name = "USER_NAME")
     public String getUserName() {
-        return userName;
+        return userName + " from getter";
     }
     public void setUserName(String userName) {
         this.userName = userName;
