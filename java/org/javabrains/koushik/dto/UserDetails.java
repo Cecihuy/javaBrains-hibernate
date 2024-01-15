e2197d89f3d98217d56ba1bab18a605707572e46
sourceTree <sourcetree@mail.com>
2024-01-15 01:01:03 -0700

    05 - More Annotations

--- java/org/javabrains/koushik/dto/UserDetails.java
+++ java/org/javabrains/koushik/dto/UserDetails.java
@@ -1,26 +1,57 @@
 package org.javabrains.koushik.dto;
-import jakarta.persistence.Column;
+import java.util.Date;
+import jakarta.persistence.Basic;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
 
-@Entity(name = "USER_DETAILS")
+@Entity
+@Table(name = "USER_DETAILS")
 public class UserDetails{    
-    private int userId;    
-    private String userName;
-
     @Id
-    @Column(name = "USER_ID")
+    private int userId;
+    @Basic
+    private String userName;
+    @Temporal(TemporalType.DATE)
+    private Date joinedDate;
+    @Transient
+    private String address;
+    @Lob
+    private String Description;
+
     public int getUserId() {
         return userId;
     }
     public void setUserId(int userId) {
         this.userId = userId;
     }
-    @Column(name = "USER_NAME")
     public String getUserName() {
-        return userName + " from getter";
+        return userName;
     }
     public void setUserName(String userName) {
         this.userName = userName;
     }
+    
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        address = address;
+    }
+    public Date getJoinedDate() {
+        return joinedDate;
+    }
+    public void setJoinedDate(Date joinedDate) {
+        this.joinedDate = joinedDate;
+    }
+    public String getDescription() {
+        return Description;
+    }
+    public void setDescription(String description) {
+        Description = description;
+    }        
 }
--- java/org/koushik/hibernate/HibernateTest.java
+++ java/org/koushik/hibernate/HibernateTest.java
@@ -1,4 +1,5 @@
 package org.koushik.hibernate;
+import java.util.Date;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -7,8 +8,11 @@ import org.javabrains.koushik.dto.UserDetails;
 public class HibernateTest {
     public static void main(String[] args) {
         UserDetails userDetails = new UserDetails();
-        userDetails.setUserId(3);
-        userDetails.setUserName("Third User");
+        userDetails.setUserId(1);
+        userDetails.setUserName("First User");
+        userDetails.setAddress("First User address");
+        userDetails.setJoinedDate(new Date());
+        userDetails.setDescription("Description of the user goes here");
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
