c64de8e675836bc1dba1ead534b8aaf269a59950
sourceTree <sourcetree@mail.com>
2024-01-16 01:57:45 -0700

    mappedBy

--- java/org/javabrains/koushik/dto/Vehicle.java
+++ java/org/javabrains/koushik/dto/Vehicle.java
@@ -3,6 +3,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -12,6 +13,7 @@ public class Vehicle {
     private int vehicleId;
     private String vehicleName;
     @ManyToOne
+    @JoinColumn(name = "USER_ID")
     private UserDetails user;
 
     public UserDetails getUser() {
