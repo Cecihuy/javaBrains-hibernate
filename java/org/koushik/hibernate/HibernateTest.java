5957ba41ef14924aaa8cf1bcb81400526b8c55d5
sourceTree <sourcetree@mail.com>
2024-01-17 01:08:20 -0700

    22  - Transient, Persistent and Detached Objects

--- java/org/koushik/hibernate/HibernateTest.java
+++ java/org/koushik/hibernate/HibernateTest.java
@@ -7,14 +7,18 @@ import org.javabrains.koushik.dto.UserDetails;
 public class HibernateTest {
     public static void main(String[] args) {        
 
+        UserDetails userDetails = new UserDetails();
+        userDetails.setUserName("Test User");   //transient
+
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
 
-        session.beginTransaction();
-        UserDetails userDetails= session.get(UserDetails.class, 5);
-        userDetails.setUserName("Updated User");
-        session.merge(userDetails);
+        session.beginTransaction();        
+        session.persist(userDetails);
+        userDetails.setUserName("Updated User again");
+        userDetails.setUserName("Last Updated User");   //persist
         session.getTransaction().commit();
+        userDetails.setUserName("Detach object can't be save after session close"); //detach
 
         session.close();
     }
