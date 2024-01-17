2e8fb09b27eb44d19f9a5c0370d46d1eab7a1952
sourceTree <sourcetree@mail.com>
2024-01-17 03:37:00 -0700

    25 - Introducing HQL and the Query Object

--- java/org/koushik/hibernate/HibernateTest.java
+++ java/org/koushik/hibernate/HibernateTest.java
@@ -1,7 +1,10 @@
 package org.koushik.hibernate;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.javabrains.koushik.dto.UserDetails;
 
 public class HibernateTest {
@@ -9,19 +12,15 @@ public class HibernateTest {
 
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
-
+        
         session.beginTransaction();
-        UserDetails userDetails = session.get(UserDetails.class, 1);
+        // Query<UserDetails> query = session.createQuery("from UserDetails", UserDetails.class);
+        Query<UserDetails> query2 = session.createQuery("from UserDetails where userId > 5", UserDetails.class);
+        List<UserDetails> users = query2.list();
         session.getTransaction().commit();
         session.close();
 
-        userDetails.setUserName("Detached object become persist again");
-
-        session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.merge(userDetails);
-        userDetails.setUserName("set after merge doesnt track last change, i don't know why. with update method it can track last change");
-        session.getTransaction().commit();
-        session.close();
+        System.out.println("Size of list result = " + users.size());
+        System.out.println("Get username by index from result of query2 = " + users.get(1).getUserName());        
     }
 }
