ca593b504f423bb2429cfc5485ca3d7ee049dd1a
sourceTree <sourcetree@mail.com>
2024-01-17 05:12:48 -0700

    27 - Understanding Parameter Binding and SQL Injection

--- java/org/koushik/hibernate/HibernateTest.java
+++ java/org/koushik/hibernate/HibernateTest.java
@@ -3,7 +3,7 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
+import org.hibernate.query.SelectionQuery;
 import org.javabrains.koushik.dto.UserDetails;
 
 public class HibernateTest {
@@ -13,16 +13,18 @@ public class HibernateTest {
         Session session = sessionFactory.openSession();
         
         session.beginTransaction();
-        Query query = session.createQuery("select userName from UserDetails", UserDetails.class);
-        // Query query = session.createQuery("select max(userId) from UserDetails", UserDetails.class);
-        query.setFirstResult(3);
-        query.setMaxResults(2);
-        List<String> users = query.list();
+        String txtBoxId = "3";
+        String txtBoxName = "user 5";
+        //use newer method hibernate 6+ instead deprecated method
+        SelectionQuery<UserDetails> query = session.createSelectionQuery("from UserDetails where userId > :pertama and userName = :kedua", UserDetails.class);
+        query.setParameter("pertama", Integer.parseInt(txtBoxId));
+        query.setParameter("kedua", txtBoxName);
+        List<UserDetails> users = query.list();
         session.getTransaction().commit();
         session.close();
 
-        for(String efl:users){
-            System.out.println(efl);
+        for(UserDetails efl:users){
+            System.out.println(efl.getUserName());
         }
     }
 }
